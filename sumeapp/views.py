9e03468126438d5dde2f8b65bab6682158066b10
tobiolusa <olusamiracle@gmail.com>
2024-05-21 22:50:08 +0100

    resume template updated

---
+++ sumeapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from .models import Profile
+import pdfkit
+from django.http import HttpResponse 
+from django.template import loader
+import io
+
+def accept(request):
+    if request.method == "POST": 
+        name = request.POST.get('name', "")
+        email = request.POST.get('email', "")
+        phone_number = request.POST.get('phone_number', "")
+        summary = request.POST.get('summary', "")
+        degree = request.POST.get('degree', "")
+        school = request.POST.get('school', "")
+        university = request.POST.get('university', "")
+        previous_work = request.POST.get('previous_work', "")
+        skills = request.POST.get('skills', "")
+        
+        profile = Profile(name=name, email=email, phone_number=phone_number, summary=summary, degree=degree, school=school, university=university, previous_work=previous_work,skills=skills)
+        profile.save()
+    return render(request, 'sumeapp/accept.html')
+
+def resume(request, id):
+    user_profile = Profile.objects.get(pk=id)
+    template = loader.get_template('sumeapp/resume.html')
+    html = template.render({'user_profile': user_profile})
+    options = {
+        'page-size' : 'Letter',
+        'encoding' : 'UTF-8',
+        }
+    pdf = pdfkit.from_string(html, False, options)
+    response = HttpResponse(pdf,content_type='application/pdf')
+    response['Content-Dospositon'] = 'attachment'
+    filename = "resume.pdf"
+    return response
+
+
+
+    

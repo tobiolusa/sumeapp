bb384849df2ca5a8d361918720365e5782765303
tobiolusa <olusamiracle@gmail.com>
2024-05-23 11:41:24 +0100

    added railway has allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gr$!0car-&bu#x%+p5#dit78qhjlgzm3qmohfht69os=l56i!$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sumeapp-production.up.railway.app']
 
 
 # Application definition

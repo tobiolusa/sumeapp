3a0103f1e9c4a4b60e575718ad833ebea7f06f4c
tobiolusa <olusamiracle@gmail.com>
2024-05-25 17:34:11 +0100

    New UI templated implemented

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gr$!0car-&bu#x%+p5#dit78qhjlgzm3qmohfht69os=l56i!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'sumeapp-production.up.railway.app',
